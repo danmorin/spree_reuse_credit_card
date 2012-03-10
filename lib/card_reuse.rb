8b85a136b52e1758e7eea8d1d2f321a3e46a4591
Dan Morin <dan.morin@gmail.com>
2012-03-09 19:28:17 -0800

    Checking for order completed instead of payment completed

--- lib/card_reuse.rb
+++ lib/card_reuse.rb
@@ -5,7 +5,7 @@ module CardReuse
     Spree::Creditcard.
       joins(:payments => :order).
       where("#{Spree::Order.table_name}.user_id" => user.id).
-      where("#{Spree::Payment.table_name}.state" => "completed").
+      where("#{Spree::Order.table_name}.state" => "completed").
       where(["(CAST(#{cc_table}.year AS integer) > ? OR (CAST(#{cc_table}.year AS integer) = ? AND CAST(#{cc_table}.month AS integer) >= ?))", 
         Date.today.year, Date.today.year, Date.today.month]).
       where(:deleted_at => nil).
