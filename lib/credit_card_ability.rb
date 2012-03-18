94ac135c1d0a956bbb0bcdbdd3e8afdf97a53d6b
Dan Morin <dan.morin@gmail.com>
2012-03-18 16:27:11 -0700

    Not restricing to completed orders in ability

--- lib/credit_card_ability.rb
+++ lib/credit_card_ability.rb
@@ -3,11 +3,9 @@ class CreditCardAbility
   include CanCan::Ability
 
   def initialize(user)
-#    binding.pry
     can :manage, Spree::Creditcard do |cc|
       cc.payments.joins(:order).
-        where("spree_orders.user_id" => user.id).
-        where("spree_orders.state" => "completed").exists?
+        where("spree_orders.user_id" => user.id).exists?
     end
   end
 end
